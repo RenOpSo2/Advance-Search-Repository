0fb789abea647431687fd60ce0fd319e0c519f86
root <root@localhost.localdomain>
2026-04-25 07:19:22 +0000

    chore: Add GPL V3

--- github_search.cpp
+++ github_search.cpp
@@ -29,6 +29,16 @@ RUN:
 ========================================================
 */
 
+/*
+RX Search
+Copyright (C) 2026 Reno
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License v3.0.
+
+See the LICENSE file for more details.
+*/
+
 #include <iostream>
 #include <string>
 #include <vector>
